92dbbf64fe5895eab9fd28e895d0bb6ef0259b44
Svarlione <ruslan.brigockij@gmail.com>
2023-08-24 19:04:54 +0300

    Add project files.

---
+++ temperatura/Program.cs
@@ -0,0 +1,8 @@
+﻿string userInput = Console.ReadLine();
+int userText = Convert.ToInt32(userInput);
+if (userText < 0)
+    Console.WriteLine("Salta");
+else if (userText < 20)
+    Console.WriteLine("Vesu");
+else if (userText > 20)
+    Console.WriteLine("Karsta");
